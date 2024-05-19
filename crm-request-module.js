e246d5aa4582daa4c0f200c75b1b50036610112b
achursin <achursin@phoenixit.ru>
2024-05-20 00:58:26 +0500

    p

---
+++ crm-request-module.js
@@ -0,0 +1,136 @@
+const axios = require('axios');
+const { wrapper } = require('axios-cookiejar-support');
+const { CookieJar } = require('tough-cookie');
+const fs = require('fs');
+const path = require('path');
+
+let cookies = {};
+
+const conf = {
+  m_url: '',
+  m_user: '',
+  m_pass: '',
+  m_isNetCore: false,
+  m_timeout: 30 // in seconds
+};
+
+const listDirectories = (dirPath) => {
+  const directories = [];
+
+  const items = fs.readdirSync(dirPath);
+  for (const item of items) {
+    const itemPath = path.join(dirPath, item);
+    if (fs.statSync(itemPath).isDirectory()) {
+      directories.push(item);
+    }
+  }
+
+  return directories;
+};
+
+const getJsonForAuth = () => JSON.stringify({
+  UserName: conf.m_user,
+  UserPassword: conf.m_pass,
+  TimeZoneOffset: -180
+});
+
+const getAuthCookie = async () => {
+  const jar = new CookieJar();
+  const client = wrapper(axios.create({ jar, baseURL: conf.m_url }));
+
+  try {
+    const response = await client.post('/ServiceModel/AuthService.svc/Login', getJsonForAuth(), {
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    response.headers['set-cookie'].forEach(cookieStr => {
+      const [key, value] = cookieStr.split(';')[0].split('=');
+      cookies[key] = value;
+    });
+
+    return cookies;
+  } catch (error) {
+    console.error('Error fetching cookies:', error);
+    return {};
+  }
+};
+
+const setCookieHeaders = (headers) => {
+  headers.Cookie = Object.entries(cookies).map(([key, value]) => `${key}=${value}`).join('; ');
+  if (cookies.BPMCSRF) {
+    headers.Bpmcsrf = cookies.BPMCSRF;
+  }
+};
+
+const query = async (queryStr, isNeedCookie = true) => {
+  if (!conf.m_isNetCore) {
+    queryStr = `/0${queryStr}`;
+  }
+
+  const jar = new CookieJar();
+  const client = wrapper(axios.create({ jar, baseURL: conf.m_url, timeout: conf.m_timeout * 1000 }));
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept-Encoding': 'gzip, deflate, br'
+  };
+
+  if (isNeedCookie) {
+    setCookieHeaders(headers);
+  }
+
+  try {
+    const response = await client.post(queryStr, null, { headers });
+    return response.data;
+  } catch (error) {
+    console.error('Error making query:', error);
+    throw error;
+  }
+};
+
+const authorization = async () => {
+  cookies = await getAuthCookie();
+  return Object.keys(cookies).length > 0;
+};
+
+const download = async (queryStr, outputLocationPath) => {
+  if (!conf.m_isNetCore) {
+    queryStr = `/0${queryStr}`;
+  }
+
+  const jar = new CookieJar();
+  const client = wrapper(axios.create({ jar, baseURL: conf.m_url, timeout: conf.m_timeout * 1000 }));
+
+  const headers = {
+    'Accept-Encoding': 'gzip, deflate, br'
+  };
+
+  setCookieHeaders(headers);
+
+  const writer = fs.createWriteStream(outputLocationPath);
+
+  try {
+    const response = await client.post(queryStr, null, {
+      headers,
+      responseType: 'stream'
+    });
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+    });
+  } catch (error) {
+    console.error('Error downloading the file:', error);
+    throw error;
+  }
+};
+
+module.exports = {
+  authorization,
+  query,
+  download,
+  conf,
+  listDirectories
+};
---
+++ loader.js
@@ -0,0 +1,42 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const { authorization, query, download, conf, listDirectories} = require('./crm-request-module');
+
+conf.m_url = core.getInput('crmUrl');
+conf.m_user = core.getInput('user');
+conf.m_pass = core.getInput('pass');
+conf.m_isNetCore = core.getInput('isNetCore');
+const path = core.getInput('path');
+
+try {
+
+  const files = listDirectories(path);
+
+  (async () => {
+
+    const isAuthorized = await authorization();
+
+    if (isAuthorized) {
+
+      for (const file of files) {
+
+        const outputPath = path.resolve(__dirname + "/resultDir", file);
+        const url = "";
+
+        try {
+          await download(url, outputPath);
+          console.log(`Файл ${file} успешно загружен`);
+        } catch (err) {
+          core.setFailed(`Ошибка скачивания файла ${file}: ${err}`);
+        }
+      }
+    } else {
+      core.setFailed(`Авторизация не удалась :С`);
+    }
+
+  })();
+
+} catch (error) {
+  core.setFailed(error.message);
+}
